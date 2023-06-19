5af9c4d360d9cce59206ef9e4bdf0579584f01dc
alpaca0x0 <alpaca0x0.tw@gmail.com>
2023-06-19 13:07:34 +0800

    export default

--- assets/js/resp.js
+++ assets/js/resp.js
@@ -1,3 +1,5 @@
-export function object(obj){
-    return ['type','status','data','message'].every((key) => { return Object.keys(obj).includes(key); }) ? obj : false;
+export default {
+    object(obj){
+        return ['type','status','data','message'].every((key) => { return Object.keys(obj).includes(key); }) ? obj : false;
+    }
 }
