0a1df52ec5d43dc9c41f85285bb395becf4b4fdd
alpaca0x0 <alpaca0x0.tw@gmail.com>
2023-07-24 17:57:54 +0800

    remove lab

--- routers/page.php
+++ routers/page.php
@@ -3,7 +3,6 @@ Router::new(Path::page);
 
 Router::equal('/', function () { Router::redirect('/index/'); });
 Router::get('index/', function () { Router::view('index'); });
-if(DEBUG) Router::equal('lab/', function () { Router::view('lab'); });
 
 Router::view();
 
