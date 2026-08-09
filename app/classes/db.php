56d5fed305606d1bca65a0ae3c07fa75ce27d512
Alpaca0x0 <alpaca0x0.tw@gmail.com>
2026-08-09 08:01:43 +0800

    fix: `inTransaction` 判斷邏輯錯誤，導致 beginTransaction 失效.

--- app/classes/db.php
+++ app/classes/db.php
@@ -143,7 +143,7 @@ class DB{
 	}
 
 	static function beginTransaction(){
-		if(!self::$connect || !self::$connect->inTransaction()){ return false; }
+		if(!self::$connect || self::$connect->inTransaction()){ return false; }
 		return self::$connect->beginTransaction();
 	}
 	static function commit(){
@@ -151,7 +151,7 @@ class DB{
 		return self::$connect->commit();
 	}
 	static function rollback(){
-		if(!self::$connect || !self::$connect->inTransaction()){ return false; }
+		if(!self::$connect || self::$connect->inTransaction()){ return false; }
 		return self::$connect->rollback();
 	}
 }
